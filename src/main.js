23b0de90786feb32e90a99cca8351269f4c2f076
sam.yang <sam.yang@itnsl.net>
2020-07-08 16:54:26 +0800

    安裝vue-axios jquery popper

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 //##下面放自己寫的
 import router from './router';
 // import routers from './router/index.js';
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
 
 new Vue({
   // router: routers//填入屬性值
