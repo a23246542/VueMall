a5396494ff372a6d9cc62c86af08cd8a74d85d8f
a23246542 <a23246542@gmail.com>
2020-07-16 02:13:12 +0800

    拆分modal 1

---
+++ src/components/DashProductModal.vue
@@ -0,0 +1,169 @@
+<template>
+  <div id="productModal" class="modal fade" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 id="exampleModalLabel" class="modal-title">
+            <span>新增產品</span>
+          </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-4">
+              <!-- <template v-if="isNewImg == 'edit'"> -->
+              <template v-if="false">
+                <!-- <div class="form-group"> -->
+                <!-- <label for="imageUrl">輸入圖片網址</label> -->
+                <!--@@沒辦法寫入麻 <input id="imageUrl" v-model="tempProduct.imageUrl[0]" type="text" -->
+                <!-- <input id="imageUrl" v-model="tempImgUrl" type="text"
+                        class="form-control" placeholder="請輸入圖片連結"> -->
+                <!-- <input id="imageUrl" v-model="tempImgUrl" type="text"
+                        class="form-control" placeholder="請輸入圖片連結"> -->
+                <!-- </div> -->
+                <div v-for="(url,index) in tempProduct.imageUrl" :key="url">
+                  <div class="form-group">
+                    <label for="imageUrl">輸入圖片網址</label>
+                    <!-- <input id="imageUrl" v-model="url" type="text"
+                            class="form-control" placeholder="請輸入圖片連結">%%會報錯 -->
+                    <input id="imageUrl" v-model="tempProduct.imageUrl[index]" type="text"
+                      class="form-control" placeholder="請輸入圖片連結">
+                  </div>
+                  <!-- <img class="img-fluid" :src="tempProduct.imageUrl" alt> -->
+                  <!-- <img class="img-fluid" :src="tempImgUrl" alt> -->
+                  <!-- <img class="img-fluid" :src="tempProduct.imageUrl[0]" alt> -->
+                  <!-- ##陣列就是v-for -->
+                  <!-- @@不知道為什麼輸入會卡卡的 -->
+                  <!-- @@tempProduct裡面的陣列沒有預先定義或set進去 卻可以響應畫面 -->
+                  <img class="img-fluid" :src=url alt>
+                </div>
+
+              </template>
+
+              <!-- <template v-if="isNewImg == 'new'"> -->
+              <template v-if="true">
+                <!-- <div v-for="(url,index) in tempProduct.imageUrl" :key="url">
+                        <div class="form-group">
+                        <label for="imageUrl">輸入圖片網址</label>
+                        <input id="imageUrl" v-model="tempProduct.imageUrl[index]" type="text"
+                        class="form-control" placeholder="請輸入圖片連結">
+                        </div>
+                        <img class="img-fluid" :src="tempProduct.imageUrl[index]" alt>
+                    </div> -->
+                <!-- -
+                    <div v-for="(item) in tempArrImgUrl" :key="item">
+                        <div class="form-group">
+                        <label for="imageUrl">輸入圖片網址</label>
+                        <input id="imageUrl" v-model="item.imgUrl" type="text"
+                        class="form-control" placeholder="請輸入圖片連結">
+                        </div>
+                        <img class="img-fluid" :src="item.imgUrl" alt>
+                    </div>
+                    - -->
+                <!-- @@index當作key有雷? -->
+                <button class="btn btn-primary" @click="addImg">新增照片</button>
+                <div v-for="(url,index) in tempProduct.imageUrl" :key="index">
+                  <div class="form-group">
+                    <label for="imageUrl">輸入圖片網址</label>
+                    <input id="imageUrl" v-model="tempProduct.imageUrl[index]" type="text"
+                      class="form-control" placeholder="請輸入圖片連結">
+                  </div>
+                  <!-- <img class="img-fluid" :src="url" alt> -->
+                  <!-- <img class="img-fluid" :src="tempProduct.imageUrl[index]" alt> -->
+                  <!-- ##不用用像v-model那個方式 -->
+                  <img class="img-fluid" :src="url" alt>
+                </div>
+
+              </template>
+            </div>
+            <div class="col-sm-8">
+              <div class="form-group">
+                <label for="title">標題</label>
+                <input id="title" v-model="tempProduct.title" type="text" class="form-control"
+                  placeholder="請輸入標題">
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="category">分類</label>
+                  <input id="category" v-model="tempProduct.category" type="text"
+                    class="form-control" placeholder="請輸入分類">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="price">單位</label>
+                  <input id="unit" v-model="tempProduct.unit" type="unit" class="form-control"
+                    placeholder="請輸入單位">
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="origin_price">原價</label>
+                  <input id="origin_price" v-model="tempProduct.origin_price" type="number"
+                    class="form-control" placeholder="請輸入原價">
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="price">售價</label>
+                  <input id="price" v-model="tempProduct.price" type="number" class="form-control"
+                    placeholder="請輸入售價">
+                </div>
+              </div>
+              <hr>
+
+              <div class="form-group">
+                <label for="description">產品描述</label>
+                <textarea id="description" v-model="tempProduct.description" type="text"
+                  class="form-control" placeholder="請輸入產品描述">
+                      </textarea>
+              </div>
+              <div class="form-group">
+                <label for="content">說明內容</label>
+                <textarea id="description" v-model="tempProduct.content" type="text"
+                  class="form-control" placeholder="請輸入說明內容">
+                      </textarea>
+              </div>
+              <div class="form-group">
+                <div class="form-check">
+                  <input id="is_enabled" v-model="tempProduct.is_enabled" class="form-check-input"
+                    type="checkbox" :true-value="1" :false-value="0">
+                  <label class="form-check-label" for="is_enabled">是否啟用</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary" data-dismiss="modal"
+            @click="cancelUpdateProduct">
+            取消
+          </button>
+          <button type="button" class="btn btn-primary" @click="updateProduct">
+            確認
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    //   name:'dash'
+    props:{
+      
+    },
+    data() {
+      return {
+        
+      }
+    },
+  }
+
+</script>
+
+<style lang="scss">
+
+</style>
