64a03b26d5d6ca03bfaa88ef38017a75983832c9
sam.yang <sam.yang@itnsl.net>
2020-07-16 18:38:03 +0800

    補

---
+++ src/components/DashDelProductModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="delProductModal" class="modal fade" tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-danger text-white">
+          <h5 id="exampleModalLabel" class="modal-title">
+            <span>刪除產品</span>
+          </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          是否刪除
+          <strong class="text-danger">{{ tempProduct.title }}</strong> 商品(刪除後將無法恢復)。
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-outline-secondary" data-dismiss="modal"
+            @click="cancelUpdateProduct">
+            取消
+          </button>
+          <button type="button" class="btn btn-danger" @click="delProduct">
+            確認刪除
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+      props:{
+        tempProduct:{}
+      }
+  }
+
+</script>
+
+<style>
+
+</style>
