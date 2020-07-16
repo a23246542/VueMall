ad792382bd0cbe6e58e3ba75d6d62e0aa4d84d34
sam.yang <sam.yang@itnsl.net>
2020-07-16 18:37:14 +0800

    拆分delProductModal 1

--- src/views/back/dashProducts.vue
+++ src/views/back/dashProducts.vue
@@ -53,46 +53,23 @@
     :is-new="isNew"
     @update="getProducts"
     />
-
-    <div id="delProductModal" class="modal fade" tabindex="-1" role="dialog"
-      aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content border-0">
-          <div class="modal-header bg-danger text-white">
-            <h5 id="exampleModalLabel" class="modal-title">
-              <span>刪除產品</span>
-            </h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            是否刪除
-            <strong class="text-danger">{{ tempProduct.title }}</strong> 商品(刪除後將無法恢復)。
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal"
-            @click="cancelUpdateProduct"
-            >
-              取消
-            </button>
-            <button type="button" class="btn btn-danger" @click="delProduct">
-              確認刪除
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <DashDelProductModal
+    :temp-product="tempProduct"
+    />
+    
  </div>
 </template>
 <script>
 import $ from "jquery";
-import DashProductModal from '../../components/DashProductModal'
+import DashProductModal from '../../components/DashProductModal.vue';
+import DashDelProductModal from '../../components/DashDelProductModal.vue';
+
 import {instanceAdmin} from '../../api/https';
 
 export default {
     components:{
-        DashProductModal
+        DashProductModal,
+        DashDelProductModal
     },
     data() {
         return {
@@ -190,7 +167,7 @@ export default {
                     break;
                 case 'delete':
                     this.tempProduct = JSON.parse(JSON.stringify(item));
-                    $('#delProductModal').modal('show');
+                    // $('#delProductModal').modal('show');
                     break;
                 default:
                     break;
