f71f91c5ff291641da42a07d76cb23bb2d963db8
sam.yang <sam.yang@itnsl.net>
2020-09-15 18:39:24 +0800

    品牌介紹

--- src/views/front/aboutUs.vue
+++ src/views/front/aboutUs.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
+  <div class="wrapper">
     <h2>品牌介紹</h2>
+    <section class="brandStory">
+      <div class="container position-relative">
+        <div class="row">
+          <div class="col-lg-5 col-md-12">
+            <h3 class="brandStory__title mt-5">
+              嚴選四大主題市集
+            </h3>
+            <h3 class="brandStory__title subTitle">
+              風格美食x裝備選物
+              <!-- 風格美食xGQ肉食節x潮流二手x裝備選物 -->
+            </h3>
+            <p class="brandStory__content mt-4 py-lg-5 pl-lg-5 pr-lg-4 pl-md-3 pr-md-3 py-3 px-3">
+              一年一度的GQ城市野營嘉年華再度回歸！今年，2019 GQ城市野營嘉年華邁入第四屆，將於2019年10月19日台北市華中露營場舉辦，除了 GQ城市野營嘉年華每年必備的風格露營體驗、兼具潮流與食尚的主題市集、現場演出的音樂舞台和各式玩樂體驗外，今年更攜手各大風格露營與選物品牌，打造野營限定商品企劃，讓戶外生活愛好者一手打包最具風格品味的野營好物！ 接下來，就先帶各位一窺2019年GQ城市野營嘉年華的神秘面貌吧！
+            </p>
+          </div>
+          <div class="col-lg-7 col-md-12">
+            <div class="brandStory__mainPic">
+              <img src="https://media.gq.com.tw/photos/5dbc1eac8569400008312f03/master/w_1280%2cc_limit/2019091354862129.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <img class="brandStory__subPic" src="https://media.gq.com.tw/photos/5dbc1eacde83b50008974ef7/master/w_1280%2cc_limit/2019091354873721.jpg" alt="">
+      </div>
+    </section>
+  </div>
   </div>
 </template>
 
