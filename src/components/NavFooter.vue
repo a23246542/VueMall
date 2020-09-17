f3be63be8d68df55dcd882a3116ece8c43a28cad
a23246542 <a23246542@gmail.com>
2020-09-18 02:24:56 +0800

    footer切版

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,7 +1,90 @@
 <template>
-  <div>
-    NavFooter
-  </div>
+  <footer class="navFooter">
+    <div class="container navFooter__container ">
+      <div class="navFooter__logo outline">
+        <img src="" alt="">
+      </div>
+      <div class="navFooter__links">
+        <ul>
+          <li class="navFooter__link">
+            <div class="footerLink">
+              <div class="footerLink-title">
+                關於我們
+              </div>
+              <ul>
+                <li class="footerLink-txt">
+                  品牌故事
+                </li>
+                <li class="footerLink-txt">
+                  最新消息
+                </li>
+                <li class="footerLink-txt">
+                  聯絡我們
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="navFooter__link">
+            <div class="footerLink">
+              <div class="footerLink-title">
+                購物須知
+              </div>
+              <ul>
+                <li class="footerLink-txt">
+                  付款方式
+                </li>
+                <li class="footerLink-txt">
+                  運送方式
+                </li>
+                <li class="footerLink-txt">
+                  退換貨方式
+                </li>
+                <li class="footerLink-txt">
+                  常見Q&A
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="navFooter__link">
+            <div class="footerLink">
+              <div class="footerLink-title">
+                條款政策
+              </div>
+              <ul>
+                <li class="footerLink-txt">
+                  網站條款
+                </li>
+                <li class="footerLink-txt">
+                  隱私權政策
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="navFooter__link">
+            <div class="footerLink">
+              <div class="footerLink-title">
+                訂閱追蹤
+              </div>
+              <ul class="footerLink__iconBlock">
+                <li class="footerLink__icon">
+                  <img src="/icons/facebook.png" alt="">
+                </li>
+                <li class="footerLink__icon">
+                  <img src="/icons/line.png" alt="">
+                </li>
+                <li class="footerLink__icon">
+                  <img src="/icons/instagram.png" alt="">
+                </li>
+                <li class="footerLink__icon">
+                  <img src="/icons/youtube.png" alt="">
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
